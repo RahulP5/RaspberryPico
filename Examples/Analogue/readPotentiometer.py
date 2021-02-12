43d91a48ea0b7d7a855505307b70477d706e6adc
Rahul Pachpind <56214015+RahulP5@users.noreply.github.com>
2021-02-12 15:03:21 +0530

    Update readPotentiometer.py

--- Examples/Analogue/readPotentiometer.py
+++ Examples/Analogue/readPotentiometer.py
@@ -2,6 +2,12 @@ import machine
 import utime
 
 potentiometer = machine.ADC(26)
+conversion_factor = 3.3 / (65535)
+
 while True:
   print(potentiometer.read_u16())
+  utime.sleep(2)
+  # print voltage
+  voltage = potentiometer.read_u16() * conversion_factor
+  print(voltage)
   utime.sleep(2)
